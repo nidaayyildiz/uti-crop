154321bf3975f3c1667a23c85868397e00b95b42
Asus <nidanurayyildiz.2@gmail.com>
2025-07-25 17:47:43 +0300

    feat: AbsoluteCrop Update

--- src/executors/AbsoluteCrop.py
+++ src/executors/AbsoluteCrop.py
@@ -11,7 +11,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
-from components.Crop.src.utils.response import build_response
+from components.Crop.src.utils.response import build_response_AbsoluteCrop
 from components.Crop.src.models.PackageModel import PackageModel
 
 
@@ -19,41 +19,30 @@ class AbsoluteCrop(Component):
     def __init__(self, request, bootstrap):
         super().__init__(request, bootstrap)
         self.request.model = PackageModel(**(self.request.data))
-        self.rotation_degree = self.request.get_param("Degree")
-        self.keep_side = self.request.get_param("KeepSide")
+        self.XCenterAbsolute = self.request.get_param("XCenterAbsolute")
+        self.YCenterAbsolute = self.request.get_param("YCenterAbsolute")
+        self.WidthAbsolute = self.request.get_param("WidthAbsolute")
+        self.HeightAbsolute = self.request.get_param("HeightAbsolute")
         self.image = self.request.get_param("inputImage")
 
     @staticmethod
     def bootstrap(config: dict) -> dict:
         return {}
 
-    def rotation(self, image):
-        if self.keep_side == True:
-            height, width = image.shape[:2]
-            image_center = (width / 2, height / 2)
-            rotation_arr = cv2.getRotationMatrix2D(image_center, self.rotation_degree, 1)
-            abs_cos = abs(rotation_arr[0, 0])
-            abs_sin = abs(rotation_arr[0, 1])
-            bound_w = int(height * abs_sin + width * abs_cos)
-            bound_h = int(height * abs_cos + width * abs_sin)
-            rotation_arr[0, 2] += bound_w / 2 - image_center[0]
-            rotation_arr[1, 2] += bound_h / 2 - image_center[1]
-            img_rotation = cv2.warpAffine(image, rotation_arr, (bound_w, bound_h))
-
-            return img_rotation
-
-        elif self.keep_side == False:
-            height, width = image.shape[:2]
-            rotation_arr = cv2.getRotationMatrix2D((height / 2, width / 2), self.rotation_degree, 1)
-            img_rotation = cv2.warpAffine(image, rotation_arr, (height, width))
-
-            return img_rotation
+    def absolute_static_crop( img: Image, x_center: float, y_center: float,width: float, height: float,):
+        x_min = round(x_center - width / 2)
+        y_min = round(y_center - height / 2)
+        x_max = round(x_min + width)
+        y_max = round(y_min + height)
+        cropped_image = img.numpy_image[y_min:y_max, x_min:x_max]
+        if not cropped_image.size:
+            return None
 
     def run(self):
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
-        img.value = self.rotation(img.value)
+        img.value = self.absolute_static_crop(img.value)
         self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
-        packageModel = build_response(context=self)
+        packageModel = build_response_AbsoluteCrop(context=self)
         return packageModel
 
 
