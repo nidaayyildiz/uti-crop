9c107534912964ddf4261e18cd4bbea8b8202958
Asus <nidanurayyildiz.2@gmail.com>
2025-07-26 23:15:22 +0300

    feat: response Update

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,13 +1,13 @@
-from param import Dynamic
+
 from sdks.novavision.src.helper.package import PackageHelper
-from components.Crop.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor,AbsoluteCropOutputs, DynamicCropOutputs, RelativeCropOutputs, AbsoluteCropResponse, DynamicCropResponse, RelativeCropResponse, AbsoluteCropExecutor, DynamicCropExecutor, RelativeCropExecutor, OutputImage
+from components.Crop.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor,AbsoluteCropOutputs, DynamicCropOutputs, RelativeCropOutputs, AbsoluteCropResponse, DynamicCropResponse, RelativeCropResponse, AbsoluteCrop, DynamicCrop, RelativeCrop, OutputImage
 
 
 def build_response_AbsoluteCrop(context):
     outputImage = OutputImage(value=context.image)
     absoluteCropOutputs = AbsoluteCropOutputs(outputImage=outputImage)
     absoluteCropResponse = AbsoluteCropResponse(outputs=absoluteCropOutputs)
-    absoluteCropExecutor = AbsoluteCropExecutor(value=absoluteCropResponse)
+    absoluteCropExecutor = AbsoluteCrop(value=absoluteCropResponse)
     executor = ConfigExecutor(value=absoluteCropExecutor)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
@@ -18,7 +18,7 @@ def build_response_DynamicCrop(context):
     outputImage = OutputImage(value=context.image)
     dynamicCropOutputs = DynamicCropOutputs(outputImage=outputImage)
     dynamicCropResponse = DynamicCropResponse(outputs=dynamicCropOutputs)
-    dynamicCropExecutor = DynamicCropExecutor(value=dynamicCropResponse)
+    dynamicCropExecutor = DynamicCrop(value=dynamicCropResponse)
     executor = ConfigExecutor(value=dynamicCropExecutor)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
@@ -30,7 +30,7 @@ def build_response_RelativeCrop(context):
     outputImage = OutputImage(value=context.image)
     relativeCropOutputs = RelativeCropOutputs(outputImage=outputImage)
     relativeCropResponse = RelativeCropResponse(outputs=relativeCropOutputs)
-    relativeCropExecutor = RelativeCropExecutor(value=relativeCropResponse)
+    relativeCropExecutor = RelativeCrop(value=relativeCropResponse)
     executor = ConfigExecutor(value=relativeCropExecutor)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
