488a581c6549d43e251577178379e34277c7a466
Asus <nidanurayyildiz.2@gmail.com>
2025-07-30 17:39:33 +0300

    feat: RelativeCrop Update

--- src/executors/RelativeCrop.py
+++ src/executors/RelativeCrop.py
@@ -13,7 +13,9 @@ from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
 from components.Crop.src.utils.response import build_response_RelativeCrop
 from components.Crop.src.models.PackageModel import PackageModel
+from sdks.novavision.src.base.logger import LoggerManager
 
+logger = LoggerManager()
 
 class RelativeCrop(Component):
     def __init__(self, request, bootstrap):
@@ -30,21 +32,25 @@ class RelativeCrop(Component):
         return {}
 
     def relative_static_crop(self, img):
-        x_center = round(img.shape[1] * self.xCenterRelative)
-        y_center = round(img.shape[0] * self.yCenterRelative)
-        width = round(img.shape[1] * self.widthRelative)
-        height = round(img.shape[0] * self.heightRelative)
+        try:
+            x_center = round(img.shape[1] * self.xCenterRelative)
+            y_center = round(img.shape[0] * self.yCenterRelative)
+            width = round(img.shape[1] * self.widthRelative)
+            height = round(img.shape[0] * self.heightRelative)
 
-        x_min = round(x_center - width / 2)
-        y_min = round(y_center - height / 2)
-        x_max = round(x_min + width)
-        y_max = round(y_min + height)
+            x_min = round(x_center - (width / 2))
+            y_min = round(y_center - (height / 2))
+            x_max = round(x_min + width)
+            y_max = round(y_min + height)
 
-        cropped_image = img[y_min:y_max, x_min:x_max]
+            cropped_image = img[y_min:y_max, x_min:x_max]
 
-        if not cropped_image.size:
-            return None
-
+            if not cropped_image.size:
+                return None
+        except TypeError as e:
+            logger.info("x_center value >= width / 2 value or y_center value >= height / 2 value")
+            logger.error(f"Error in cropping: {e}")
+            return img
         return cropped_image
 
     def run(self):
